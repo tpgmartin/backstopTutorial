e50be463fbb544dd831d97a0069e390f0cc18c48
Tom Martin <tom@tpgmartin.com>
2014-12-23 14:36:26 +0000

    Find all css selectors

---
+++ dist/js/parse.js
@@ -0,0 +1,30 @@
+var Elements = document.body.getElementsByTagName('*');
+var selectorNames = {};
+for (var elem = 0, max = Elements.length; elem < max; elem++) {
+  var selector = Elements[elem];
+  // Check for classes
+  if (selector.className) {
+    var classes = selector.className.split(" ");
+    for (var cn = 0; cn < classes.length; cn++){
+      var cName = classes[cn];
+      if (!selectorNames[cName]) {
+        selectorNames[cName] = true;
+      }
+    }
+  }   
+  // Check for ids
+  if (selector.id) {
+    if (!selectorNames[selector.id]) {
+      selectorNames[selector.id] = true;
+    }
+  }
+  // Check for tag names
+  if (selector.tagName) {
+    var tagName = selector.tagName.toLowerCase();
+    if (!selectorNames[tagName]) {
+      selectorNames[tagName] = true;
+    }
+  }
+}
+var classList = [];
+for (var name in selectorNames) classList.push(name);
